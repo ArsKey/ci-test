52e4b90f543097bfe72b708ab29b7af1ee474550
Ars <ars.tuilindo@gmail.com>
2022-04-21 00:15:02 +0300

    great code

---
+++ src/main/java/com/ars/java/automation/App.java
@@ -0,0 +1,13 @@
+package com.ars.java.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" + args[0]);
+    }
+}
