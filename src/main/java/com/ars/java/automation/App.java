92c5ae250158d8e2eda9728999b560b5ab05cc37
Ars <ars.tuilindo@gmail.com>
2022-04-21 02:54:28 +0300

    cobertura

--- src/main/java/com/ars/java/automation/App.java
+++ src/main/java/com/ars/java/automation/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" + args[0]);
+        System.out.println( "Hello World!");
     }
 
 
